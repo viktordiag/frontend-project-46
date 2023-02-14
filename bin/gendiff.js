ac91a531949e9d19b0f2374339e0ea8b8de7d1ae
viktordiag <viktordiag@gmail.com>
2023-02-14 22:37:58 +0300

    end step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,9 +2,20 @@
 
 import { program } from 'commander'
 
+const command = (filepath1, filepath2) => {
+	if (program.opts().format) {
+		console.log(`${filepath1} and ${program.opts().format}`);
+	} else {
+	console.log(filepath2);
+	}
+};
+
 program
 	.version('0.0.1')
+	.helpOption('-h, --help', 'output usage information')
 	.description('Compares two configuration files and shows a difference.')
+	.arguments('<filepath1> <filepath2>')
+	.option('-f, --format <type>', 'output format')
+	.action(command) 
 	.parse();
 
-console.log('hello world');
