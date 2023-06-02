9e73f72b578d38d80fba423bccb6a592678bbb87
viktordiag <viktordiag@gmail.com>
2023-06-02 20:26:50 +0300

    add

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { program } from 'commander'
-import { dataFile } from '../src/index.js'
+import { program } from 'commander';
+import { dataFile } from '../src/index.js';
 
 program
   .version('0.0.1')
