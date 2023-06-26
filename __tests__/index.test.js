68a8543d6e933e7bccd591c40ad24a4d59ac9baf
Viktor Aleksandrov <viktordiag@gmail.com>
2023-06-27 00:01:32 +0300

    add new action

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,10 +11,16 @@ const readfile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 const filePath1 = getFixturePath('file1.json');
 const filePath2 = getFixturePath('file2.json');
+const filePath3 = getFixturePath('file3.yaml');
+const filePath4 = getFixturePath('file4.yaml');
 const fileOutJson = readfile('fileOutJson.txt');
+const fileOutYaml = readfile('fileOutYaml.txt');
 
 describe('compare files', () => {
   test('it compare json files', () => {
     expect(gendiff(filePath1, filePath2)).toEqual(fileOutJson);
+  });
+  test('it compare yaml files', () => {
+    expect(gendiff(filePath3, filePath4)).toEqual(fileOutYaml);
   });
 });
